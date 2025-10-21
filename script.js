f8dad058795ed4cf7e6320e98ced86a8bce6efab
zezorbao <nascimento.araujo.maria@escola.pr.gov.br>
2025-10-21 08:30:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector(.ativo);
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        ativo.classList.remove('ativo');
+    
+    })
+})
